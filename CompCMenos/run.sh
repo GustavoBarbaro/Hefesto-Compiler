9e1d2d8bc80fef1f42abfb44fb37966c78713345
Gustavo <gustavobarbarooliveira@hotmail.com>
2021-10-28 20:17:32 -0300

    tirei o ly

--- CompCMenos/run.sh
+++ CompCMenos/run.sh
@@ -2,5 +2,5 @@
 bison -d parse.y
 flex scan.l 
 gcc -c lex.yy.c
-gcc main.c lex.yy.o parse.tab.c definitions.c symtab.c analyze.c cgen.c assembly.c binario.c -o exec -lfl -ly
+gcc main.c lex.yy.o parse.tab.c definitions.c symtab.c analyze.c cgen.c assembly.c binario.c -o exec -lfl
 ./exec
