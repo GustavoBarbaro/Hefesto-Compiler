886dbfd70eb0f4fdf54b5be34c92aad7ee78821e
Gustavo <gustavobarbarooliveira@hotmail.com>
2022-02-20 22:16:41 -0300

    update `sample.c`

--- CompCMenos/sample.c
+++ CompCMenos/sample.c
@@ -1,9 +1,16 @@
 void main(void) { 
     int x; 
-    x = 1;
-    while (x <= 4){
+    int resp;
+
+    /* calcula 5^4 = 625 */
+
+    x = 0;
+    resp = 1;
+
+    while (x < 4){
+        resp = resp * 5;
         x = x + 1;
+        output(resp);
     }
-    output(x);
-    
+    output(resp);
 }
